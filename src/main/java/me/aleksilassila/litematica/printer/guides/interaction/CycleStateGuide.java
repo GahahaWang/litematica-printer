1b7827549c47b54f2968e6a1e6d0f8fcdf3a0516
Gahaha <luke921021luke@gmail.com>
2026-03-30 05:43:11 +0800

    fix: printing Redundant iron trapdoor & iron door since they aren't interactable

--- src/main/java/me/aleksilassila/litematica/printer/guides/interaction/CycleStateGuide.java
+++ src/main/java/me/aleksilassila/litematica/printer/guides/interaction/CycleStateGuide.java
@@ -3,6 +3,7 @@ package me.aleksilassila.litematica.printer.guides.interaction;
 import me.aleksilassila.litematica.printer.SchematicBlockState;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LeverBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -14,8 +15,13 @@ import java.util.List;
 public class CycleStateGuide extends InteractionGuide {
     private static final Property<?>[] propertiesToIgnore = new Property[]{
             BlockStateProperties.POWERED,
-            BlockStateProperties.LIT,
             BlockStateProperties.LOCKED,
+            BlockStateProperties.NOTEBLOCK_INSTRUMENT,
+            BlockStateProperties.POWER
+    };
+    private static final Property<?>[] propertiesToIgnore2 = new Property[]{
+            BlockStateProperties.POWERED,
+            BlockStateProperties.OPEN,
     };
 
     public CycleStateGuide(SchematicBlockState state) {
@@ -39,6 +45,9 @@ public class CycleStateGuide extends InteractionGuide {
     protected boolean statesEqual(BlockState state1, BlockState state2) {
         if (state2.getBlock() instanceof LeverBlock) {
             return super.statesEqual(state1, state2);
+        }
+        if (state2.is(Blocks.IRON_DOOR) || state2.is(Blocks.IRON_TRAPDOOR)) {
+            return statesEqualIgnoreProperties(state1, state2, propertiesToIgnore2);
         }
 
         return statesEqualIgnoreProperties(state1, state2, propertiesToIgnore);
